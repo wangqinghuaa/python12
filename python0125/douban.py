001a3e03dc488ea9feb1d881f5c1f7ef8f2b3f24
wangqinghuaa <451416191@qq.com>
2022-01-22 23:43:32 +0800

    11

--- python0125/douban.py
+++ python0125/douban.py
@@ -13,7 +13,7 @@ url="https://book.douban.com/chart?subcat=F&icn=index-topchart-fiction"
 myheader={"User-Agent":"Chrome/96.0.4664.45"}
 result=requests.get(url,headers=myheader)
 html=result.text
-# print(html)
+
 soup=BeautifulSoup(html,"lxml")#需要传递2个参数
 r=soup.find_all("a",class_="fleft")
 book_list=soup.find_all("li",class_="media")
