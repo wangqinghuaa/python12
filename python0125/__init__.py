4241656da8c5e824169a1edd67697b9c9991a1d0
wangqinghuaa <451416191@qq.com>
2022-01-22 23:29:34 +0800

    Initial commit

---
+++ python0125/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     :2022-01-22 23:28
+# @Author   :wangqinghua
+# @Email    : 867075698@qq.com
+# @File     : __init__.py.py
+# @Software : PyCharm
---
+++ python0125/douban.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     :2022-01-21 22:44
+# @Author   :wangqinghua
+# @Email    : 867075698@qq.com
+# @File     : douban.py
+# @Software : PyCharm
+
+import requests
+from bs4 import BeautifulSoup
+
+url="https://book.douban.com/chart?subcat=F&icn=index-topchart-fiction"
+myheader={"User-Agent":"Chrome/96.0.4664.45"}
+result=requests.get(url,headers=myheader)
+html=result.text
+# print(html)
+soup=BeautifulSoup(html,"lxml")#需要传递2个参数
+r=soup.find_all("a",class_="fleft")
+book_list=soup.find_all("li",class_="media")
+print(book_list)
+for book in book_list:
+    title=book.find("a",class_="fleft").text
+    author=book.find("p",class_="subject-abstract").text.strip()
+    price=book.find("span",class_="buy-info").find("a").text
+    print(title,author,price)
+    print("*"*10)
+
+
+
