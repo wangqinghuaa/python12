0d4b544571a0ef9ceb70b0e7042d782abca65ca6
wangqinghuaa <451416191@qq.com>
2022-01-23 23:55:55 +0800

    .
    Signed-off-by: wangqinghuaa <451416191@qq.com>

---
+++ python0123/fakers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     :2022-01-23 21:06
+# @Author   :wangqinghua
+# @Email    : 867075698@qq.com
+# @File     : fakers.py
+# @Software : PyCharm
+
+from faker import Faker
+from faker import Factory
+fake = Faker(locale="en_US")
+# print(fake.name())
+
+# print(dir(fake))
+# print(fake.address())
+# print(fake.phone_number())
+# print(dir(fake))
+
+# for _ in range(1):
+for _ in range(10):
+    r=('姓名：', fake.name(), ' 手机号：', fake.phone_number())
+    print(r)
